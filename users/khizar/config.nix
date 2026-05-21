3cea6b2d9d63b8451b319ed1dbe2748deafb9aff
Khizar <khizarshakeel9@gmail.com>
2026-05-21 11:35:54 -0600

    setting up autostart for equibop on boot

--- users/khizar/config.nix
+++ users/khizar/config.nix
@@ -8,7 +8,9 @@
 
     "niri/config.kdl".source          = ./dotfiles/niri/config.kdl;
     "niri/cfg/animation.kdl".source   = ./dotfiles/niri/cfg/animation.kdl;
-    "niri/cfg/autostart.kdl".source   = ./dotfiles/niri/cfg/autostart.kdl;
+    "niri/cfg/autostart.kdl".source = if hostName == "nixos-pc"
+      then ./dotfiles/niri/cfg/autostart-pc.kdl
+      else ./dotfiles/niri/cfg/autostart-laptop.kdl;
     "niri/cfg/input.kdl".source       = ./dotfiles/niri/cfg/input.kdl;
     "niri/cfg/keybinds.kdl".source    = ./dotfiles/niri/cfg/keybinds.kdl;
     "niri/cfg/layout.kdl".source      = ./dotfiles/niri/cfg/layout.kdl;
